660a4f41713414058195fd1338b9327e8462e6d5
John Melody Melissa <Johnmelodyme@yandex.com>
2019-05-04 22:14:04 +0800

    Add files via upload

---
+++ Darkweb.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Dark Web links
+
+print("[ D ]  [  A ]  [  R ]  [  K  ]     [  W ]  [ E ]  [  B  ]     [  L ]  [  I  ]  [  N  ]  [  K ] [  S  ] ")
+
+dark_web_search_engine = "grams7enufi7jmdl.onion"
+print("Search Engine:    " + dark_web_search_engine)
+
+black_market = "eajwlvm3z2lcca76.onion" 
+print("Black Market:   " + black_market)
+
+get_bitcoin = "https://www.bitcoinget.com/"
+print("Free Bitcoin:   " + get_bitcoin)
+
+books = "http://yuxv6qujajqvmypv.onion/"
+print("Download Books:     " + books)
+
+human_trafficking_girl = "http://nr6juudpp4as4gjg.onion/torgirls.html"
+print("Human Trafficking :: GIRLS ::    " + human_trafficking_girl)
+
+drug_market = "http://4yjes6zfucnh7vcj.onion/"
+print("Illegal drug dealer:    "  + drug_market)
+
+radio = "http://76qugh5bey5gum7l.onion/"
+print("Dark Web radio:     " + radio)
+
+chat = "http://kpmp444tubeirwan.onion/ "
+print("Deep Web Chat:      " +  chat)
+
+WK= "http://zbnnr7qzaxlk5tms.onion/"
+print("Wkikleaks :   " + WK)
+
+red_room = "http://k4jmdeccpnsfe43c.onion/"
+print("RED ROOM:    "  + red_room)
+
+PS = "http://tklxxs3rdzdjppnl.onion/sharepass/"
+print("Password Sharing:   " + PS)
+
+IEE = "http://rrcc5uuudhh4oz3c.onion/ "
+print("INTELLIGENCES EXCHANGE:     " + IEE)
