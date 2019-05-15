f0320a57a6afd531d13ba7abbb4d938dd11ad4b1
John Melody Melissa <Johnmelodyme@yandex.com>
2019-05-15 09:05:34 +0800

    Update Darkweb.py

--- Darkweb.py
+++ Darkweb.py
@@ -1,40 +1,80 @@
 #!/usr/bin/env python
 # Dark Web links
-
-print("[ D ]  [  A ]  [  R ]  [  K  ]     [  W ]  [ E ]  [  B  ]     [  L ]  [  I  ]  [  N  ]  [  K ] [  S  ] ")
-
-dark_web_search_engine = "grams7enufi7jmdl.onion"
+print("DARK WEB LINKS" ) # REF http://deepweblinks.org/
+dark_web_search_engine = "http://grams7enufi7jmdl.onion"
 print("Search Engine:    " + dark_web_search_engine)
-
-black_market = "eajwlvm3z2lcca76.onion" 
+black_market = "http://eajwlvm3z2lcca76.onion" 
 print("Black Market:   " + black_market)
-
 get_bitcoin = "https://www.bitcoinget.com/"
 print("Free Bitcoin:   " + get_bitcoin)
-
+pdf = "http://am4wuhz3zifexz5u.onion"
 books = "http://yuxv6qujajqvmypv.onion/"
-print("Download Books:     " + books)
-
+print("Download Books:     " + books  + "  or  " + pdf)
 human_trafficking_girl = "http://nr6juudpp4as4gjg.onion/torgirls.html"
 print("Human Trafficking :: GIRLS ::    " + human_trafficking_girl)
-
 drug_market = "http://4yjes6zfucnh7vcj.onion/"
 print("Illegal drug dealer:    "  + drug_market)
-
 radio = "http://76qugh5bey5gum7l.onion/"
 print("Dark Web radio:     " + radio)
-
 chat = "http://kpmp444tubeirwan.onion/ "
 print("Deep Web Chat:      " +  chat)
-
 WK= "http://zbnnr7qzaxlk5tms.onion/"
 print("Wkikleaks :   " + WK)
-
 red_room = "http://k4jmdeccpnsfe43c.onion/"
 print("RED ROOM:    "  + red_room)
-
 PS = "http://tklxxs3rdzdjppnl.onion/sharepass/"
 print("Password Sharing:   " + PS)
-
 IEE = "http://rrcc5uuudhh4oz3c.onion/ "
 print("INTELLIGENCES EXCHANGE:     " + IEE)
+intel = "http://intelex7ny6coqno.onion/category/6"
+intel_exchange = "https://onionnews.fandom.com/wiki/Intel_Exchange"
+print("INTELLIGENCE EXCHANGE:  " + intel_exchange + "  or  " + intel)
+tor = "http://xmh57jrzrnw6insl.onion/"
+print("TOR: " + tor)
+hacker_disscusion = "http://jv7aqstbyhd5hqki.onion"
+hacker_social = "http://bbxdfsru7lmmbj32.onion" 
+hacker_ser = "http://iir4yomndw2dec7x.onion/planet"
+hacker_network = "http://hackerrdjd6amg3l.onion/"
+ddos ="http://iir4yomndw2dec7x.onion/planet"
+print("Hacker Network: " + hacker_network)
+print("HACKER SERVICE: " + hacker_ser)
+print("DELTA INIATIATIVE: " + hacker_social)
+print("Hacker Forum: " + hacker_disscusion)
+print("DDOS : " + ddos)
+cannabis = "http://fzqnrlcvhkgbdwx5.onion/"
+print("WEED: " + cannabis)
+documents_download = "http://sblib3fk2gryb46d.onion"
+print("DOCS: " + documents_download)
+sea = "https://ahmia.fi/"
+print("Another search engine: " + sea)
+hacker_area = "http://pnxauh22krzb5xso.onion/code-like-a-hacker/"
+print("HAcker Tutorial: " + hacker_area)
+hack_instagram = "http://nc56ejhnca5uwl2dkjvjhzffhd3z7cuwk2cbi6vkfnckvk4w7kurcdyd.onion/"
+print("HAck Instagram: " + hack_instagram)
+deep_paste = "http://depastedihrn3jtw.onion/top.php"
+print("DEEPSH*T: " + deep_paste)
+l = "http://intel2iy3ky6hegf.onion/confidential%20intel.html"
+print("LEAKS: " + l)
+c = "http://cavetord6bosm3sl.onion/catalog/item478.html"
+b = "http://tinhat233xymse34.onion/"
+print("HACKER: " + c + " " + b)
+hhh = "http://depastedihrn3jtw.onion/show.php?md5=5c07493b54bfccdc67a6572f9d79b05f"
+print("Black Girls traficcking: " + hhh)
+child_porn = "http://2pximuustc5l4djzfqg3qzhr4kpb3veva7gcdrcsxu2vmrt5dbm4clad.onion/"
+cp = "http://6bqs6m6wvmxzclng.onion/free1.html"
+print("Child Porn creepy: " + child_porn + " and " + cp)
+hacker_f = "http://libraryqtlpitkix.onion/library/Fiction/"
+print("Hacker fiction: " + hacker_f)
+ch_po = "http://lisach7joohmqk3a.onion/cozy/res/37780.html"
+print("ANother child porn: "  + ch_po) #seriously what the fuck?
+search_engine_2 = "http://3bbaaaccczcbdddz.onion/"
+print("Search Engine: " + search_engine_2)
+rape_vid = "http://rapedbigpr4j7mhl.onion/"
+print("RAPE: " + rape_vid)
+int_intel = "http://qbte2zxvhjk6bftp.onion/"
+print("Best: "  + int_intel)
+tsw = "32avzir6unmcg2y2.onion"
+print("the secret world: " + tsw)
+c_pdf = "http://h2am5w5ufhvdifrs.onion/"
+x = " Classified file: "
+print(x + c_pdf)
