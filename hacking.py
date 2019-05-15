f9b02827644bfcbb3036ce8d8dd2239521dcd745
John Melody Melissa <Johnmelodyme@yandex.com>
2019-05-15 09:06:13 +0800

    Create hacking.py

---
+++ hacking.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#HACKING SITE:
+
+ATM = "https://bit.ly/2W8252c"
+AIH = "https://bit.ly/2JDtW3x"
+B_MrROBOT = "https://bit.ly/2rlCYdg"
+SD = "https://bit.ly/2E1FA4D"
+H_instagram = "http://nc56ejhnca5uwl2dkjvjhzffhd3z7cuwk2cbi6vkfnckvk4w7kurcdyd.onion/"
+anonymous_hacktivist_group = "http://society44nlbxqdz.onion/cryptyc1128"
+drMelodyme = "http://society44nlbxqdz.onion/drmelody27/all"
+BHC = "http://blkh4ylofapg42tj6ht565klld5i42dhjtysvsnnswte4xt4uvnfj5qd.onion/"
+EC ="http://evilchatxp24s7vb.onion/chat.php"
+dw_down_malware = "http://exoduockgfq3ikf7.onion"
+dw_down_malware_2 ="http://darkdev5ibekblct.onion"
+dw_down_malware_3 = "http://iec56w4ibovnb4wc.onion"
+hack_t = "http://fqnqc7zix2wblwex.onion"
+#https://www.deepwebsiteslinks.com/deep-web-weapons-software-hacking-virus-cracking-sites-links/
+print("Hacking ATM with Raspberry PI: " + ATM)
+print("Hacking AI with Raspberry PI: " + AIH)
+print("Build A Device for Hacker:: (Like MR ROBOT): " + B_MrROBOT)
+print("Building a Stalking Device: " + SD)
+print("Hack Instagram By ID: " + H_instagram)
+print("AnnonymosHackerGroup: " + anonymous_hacktivist_group + "  find me :  " + drMelodyme)
+print("BLACK HAT HACKER CHATROOM: " + BHC) 
+print("EvilChat: " + EC)
+print("Cyber weaponary: " + dw_down_malware + " or " + dw_down_malware_2 + " or " + dw_down_malware_3)
+print("HAck traffic data with these: " + hack_t)
